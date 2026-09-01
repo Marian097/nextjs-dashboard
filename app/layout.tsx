0519bc915c58d0bd51bfc2a24e96d928ac359440
Lungu Marian <141348340+Marian097@users.noreply.github.com>
2026-09-01 09:44:59 +0300

    chapter 5 from next

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { inter } from '@/app/ui/fonts';
+import './ui/global.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
